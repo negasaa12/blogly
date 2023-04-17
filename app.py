df97d46220563f505aa04fec3a90bfd2f5100282
kelly <sntsklvn@gmail.com>
2023-04-17 13:13:00 -0400

    handle form for deleting tags

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
-from datetime import datetime
-from flask import Flask, request, redirect, render_template
+from datetime import datetime 
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User, Post, PostTag, Tag
 from sqlalchemy import text
@@ -241,5 +241,44 @@ def tags_show(tag_id):
     """Show a page with info on a specific tag"""
 
     tag = Tag.query.get_or_404(tag_id)
-    
+
     return render_template('tag_details.html', tag=tag)
+
+
+@app.route('/tags/<int:tag_id>/edit')
+def tags_edit_form(tag_id):
+    """Show a form to edit an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    posts = Post.query.all()
+    return render_template('edit_tags.html', tag=tag, posts=posts)
+
+
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def tags_edit(tag_id):
+    """Handle form submission for updating an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    tag.name = request.form['name']
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+
+    db.session.add(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' edited.")
+
+    return redirect("/tags")
+
+
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def tags_destroy(tag_id):
+    """Handle form submission for deleting an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' deleted.")
+
+    return redirect("/tags")
